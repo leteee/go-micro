de7f5b2f8fbd90ee7da8cdc4c580cf2458089c26
light <leiteng712@qq.com>
2023-09-13 14:59:58 +0800

    feat: Grpc创建微服务,注册到consul

--- grpc-demo/client/main.go
+++ grpc-demo/client/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
+	"github.com/hashicorp/consul/api"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	pb "grpc-demo/greeter/greeter"
@@ -15,13 +17,23 @@ const (
 )
 
 var (
-	addr = flag.String("addr", "localhost:8000", "the address to connect to")
+	//addr = flag.String("addr", "localhost:8000", "the address to connect to")
 	name = flag.String("name", defaultName, "Name to greet")
 )
 
 func main() {
+
+	//初始化consul配置
+	consulConfig := api.DefaultConfig()
+
+	//获取consul操作对象
+	registry, _ := api.NewClient(consulConfig)
+
+	serviceEntry, _, _ := registry.Health().Service("HelloService", "testHello", false, &api.QueryOptions{})
+	addr := fmt.Sprintf("%s:%v", serviceEntry[0].Service.Address, serviceEntry[0].Service.Port)
+	fmt.Println("consul获取的地址-", addr)
 	flag.Parse()
-	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
--- grpc-demo/server/main.go
+++ grpc-demo/server/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"github.com/hashicorp/consul/api"
 	"google.golang.org/grpc"
 	pb "grpc-demo/greeter/greeter"
 	"log"
@@ -26,6 +27,28 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 }
 
 func main() {
+	//初始化consul配置
+	consulConfig := api.DefaultConfig()
+
+	//获取consul操作对象
+	registry, _ := api.NewClient(consulConfig)
+
+	//注册服务,服务的常规配置
+	registerService := api.AgentServiceRegistration{
+		ID:      "1",
+		Tags:    []string{"testHello"},
+		Name:    "HelloService",
+		Port:    *port,
+		Address: "127.0.01",
+		Check: &api.AgentServiceCheck{
+			TCP:      fmt.Sprintf("127.0.0.1:%d", *port),
+			Timeout:  "5s",
+			Interval: "5s",
+		},
+	}
+	//注册服务到consul上
+	registry.Agent().ServiceRegister(&registerService)
+
 	flag.Parse()
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
